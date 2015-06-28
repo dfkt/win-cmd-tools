179a51a6fea943fc6d5f2e2bdd744c1a602cc3dd
dfkt <github@dfkt.tk>
2015-06-29 01:43:32 +0200

    Update

--- Graphics/Rename-JPG-by-modification-date-time-POWERSHELL.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-ChildItem *.jpg | Rename-Item -newname {$_.LastWriteTime.toString('yyyy-MM-dd_HHmmss') + '.jpg'}"
-
-pause
--- Graphics/Rename-PNG-by-modification-date-time-POWERSHELL.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-powershell -NoProfile -ExecutionPolicy Bypass -Command "Get-ChildItem *.png | Rename-Item -newname {$_.LastWriteTime.toString('yyyy-MM-dd_HHmmss') + '.png'}"
-
-pause
